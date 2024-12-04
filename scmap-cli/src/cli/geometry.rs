917137616c9ef4b02a3cd767dc102a26538e3b8b
noamteyssier <22600644+noamteyssier@users.noreply.github.com>
2024-12-04 11:32:21 -0800

    feat: added conversion into ibu header

--- scmap-cli/src/cli/geometry.rs
+++ scmap-cli/src/cli/geometry.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use ibu::Header;
 use scmap::GeometryR1;
 
 #[derive(Parser)]
@@ -16,5 +17,19 @@ impl From<Geometry> for GeometryR1 {
             barcode: geometry.barcode,
             umi: geometry.umi,
         }
+    }
+}
+impl TryFrom<Geometry> for Header {
+    type Error = anyhow::Error;
+
+    #[allow(clippy::cast_possible_truncation)]
+    fn try_from(geometry: Geometry) -> Result<Self, Self::Error> {
+        let header = Header::new(
+            1,                       // IBU version
+            geometry.barcode as u32, // Barcode size
+            geometry.umi as u32,     // UMI size
+            false,                   // Sorted flag
+        )?;
+        Ok(header)
     }
 }
