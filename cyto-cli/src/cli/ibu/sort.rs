43ba91cfeccd581b27fbb95c134db6c07b23755f
noamteyssier <22600644+noamteyssier@users.noreply.github.com>
2024-12-09 17:21:54 -0800

    feat: avoid writing ibu to stdout unless piped explicitly

--- cyto-cli/src/cli/ibu/sort.rs
+++ cyto-cli/src/cli/ibu/sort.rs
@@ -5,8 +5,17 @@ pub struct ArgsSort {
     #[clap(flatten)]
     pub input: IbuInput,
 
-    #[clap(short, long)]
+    /// Output file to write to
+    ///
+    /// Required unless `-p/--pipe` is present.
+    #[clap(short, long, required_unless_present("pipe"))]
     pub output: Option<String>,
+
+    /// Pipe the output to stdout
+    ///
+    /// Due to binary output, this flag is necessary not to flood the terminal with binary.
+    #[clap(short, long, conflicts_with("output"))]
+    pub pipe: bool,
 
     #[clap(short = 't', long, default_value = "1")]
     pub num_threads: usize,
