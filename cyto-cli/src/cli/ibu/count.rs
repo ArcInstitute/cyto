9b74113b9afb42d0bfc6d83523d6b393a583c3b1
noamteyssier <22600644+noamteyssier@users.noreply.github.com>
2024-12-19 10:56:52 -0800

    feat: improve io options for count

--- cyto-cli/src/cli/ibu/count.rs
+++ cyto-cli/src/cli/ibu/count.rs
@@ -9,6 +9,11 @@ pub struct ArgsCount {
     #[clap(short, long)]
     pub output: Option<String>,
 
+    /// Number of threads to use in counting
     #[clap(short = 't', long, default_value = "1")]
     pub num_threads: usize,
+
+    /// Keep the barcode values 2bit compressed in the output
+    #[clap(short = 'e', long = "compressed")]
+    pub compressed: bool,
 }
--- cyto-cli/src/commands/ibu/count.rs
+++ cyto-cli/src/commands/ibu/count.rs
@@ -1,30 +1,59 @@
+use anyhow::Result;
+use ibu::{Header, Reader};
+use std::io::Write;
+
 use crate::{
     cli::ibu::ArgsCount,
     io::{match_input, match_output},
 };
-use anyhow::Result;
-use cyto::deduplicate_umis;
-use ibu::Reader;
+use cyto::{deduplicate_umis, BarcodeIndexCount};
+
+fn dump_encoded_records<W: Write>(
+    csv_writer: &mut csv::Writer<W>,
+    records: impl Iterator<Item = BarcodeIndexCount>,
+) -> Result<()> {
+    for record in records {
+        csv_writer.serialize(record)?;
+    }
+    csv_writer.flush()?;
+    Ok(())
+}
+
+fn decode_record(record: BarcodeIndexCount, header: Header) -> Result<(String, u64, u64)> {
+    let barcode = bitnuc::from_2bit(record.barcode(), header.barcode_len() as usize)?;
+    let barcode_str = String::from_utf8(barcode)?;
+    Ok((barcode_str, record.count(), record.index()))
+}
+
+fn dump_decoded_records<W: Write>(
+    csv_writer: &mut csv::Writer<W>,
+    records: impl Iterator<Item = BarcodeIndexCount>,
+    header: Header,
+) -> Result<()> {
+    for record in records {
+        let decoded = decode_record(record, header)?;
+        csv_writer.serialize(decoded)?;
+    }
+    csv_writer.flush()?;
+    Ok(())
+}
 
 pub fn run(args: &ArgsCount) -> Result<()> {
     let input = match_input(args.input.input.as_ref())?;
 
     let reader = Reader::new(input)?;
-    let _header = reader.header();
+    let header = reader.header();
     let counts = deduplicate_umis(reader)?;
     let output_handle = match_output(args.output.as_ref())?;
+
+    // Write output
     let mut writer = csv::WriterBuilder::new()
         .delimiter(b'\t')
         .from_writer(output_handle);
 
-    counts
-        .iter_counts()
-        .try_for_each(|count| -> Result<(), csv::Error> {
-            writer.serialize(count)?;
-            Ok(())
-        })?;
-
-    writer.flush()?;
-
-    Ok(())
+    if args.compressed {
+        dump_encoded_records(&mut writer, counts.iter_counts())
+    } else {
+        dump_decoded_records(&mut writer, counts.iter_counts(), header)
+    }
 }
