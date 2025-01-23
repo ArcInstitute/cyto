fa4b478d52614004575f814ce988a938778d4aa8
noamteyssier <22600644+noamteyssier@users.noreply.github.com>
2025-01-23 12:46:07 -0800

    fix: remove namespace conflict in cli

--- cyto-cli/src/cli/map/geometry.rs
+++ cyto-cli/src/cli/map/geometry.rs
@@ -4,7 +4,7 @@ use cyto::GeometryR1;
 #[derive(Parser)]
 #[clap(next_help_heading = "Geometry Configuration")]
 pub struct Geometry {
-    #[clap(short = 'b', long, default_value = "16")]
+    #[clap(short = 'B', long, default_value = "16")]
     pub barcode: usize,
     #[clap(short = 'u', long, default_value = "12")]
     pub umi: usize,
