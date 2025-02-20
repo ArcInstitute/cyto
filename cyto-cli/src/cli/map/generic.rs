44810eb3e94a162d012b039e49c6407bd29f2914
noamteyssier <22600644+noamteyssier@users.noreply.github.com>
2025-02-20 14:30:57 -0800

    feat: added binseq option to generic mapping

--- cyto-cli/src/cli/map/generic.rs
+++ cyto-cli/src/cli/map/generic.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use cyto::mappers::MapperOffset;
 
 use super::{Geometry, PairedInput, RuntimeOptions};
 use crate::cli::ArgsOutput;
@@ -35,13 +36,39 @@ pub struct GenericOptions {
     #[clap(short = 'c', long = "generic")]
     pub generic_filepath: String,
 
-    /// Left-boundary to extract sequence
-    #[clap(short = 's', long)]
-    pub boundary: usize,
+    /// Index to extract sequence (right of this point)
+    #[clap(
+        short = 's',
+        long,
+        conflicts_with = "left_of",
+        required_unless_present = "left_of"
+    )]
+    pub right_of: Option<usize>,
+
+    /// Index to extract sequence (left of this point)
+    #[clap(
+        short = 'S',
+        long,
+        conflicts_with = "right_of",
+        required_unless_present = "right_of"
+    )]
+    pub left_of: Option<usize>,
 
     /// Use exact matching for flex sequences and probes.
     ///
     /// Default allows for unambiguous 1-hamming distance mismatches
     #[clap(short = 'x', long)]
     pub exact_matching: bool,
+}
+
+impl GenericOptions {
+    pub fn offset(&self) -> MapperOffset {
+        if let Some(right_of) = self.right_of {
+            MapperOffset::RightOf(right_of)
+        } else if let Some(left_of) = self.left_of {
+            MapperOffset::LeftOf(left_of)
+        } else {
+            unreachable!("This should never happen")
+        }
+    }
 }
--- cyto-cli/src/commands/map/generic.rs
+++ cyto-cli/src/commands/map/generic.rs
@@ -1,7 +1,99 @@
-use anyhow::Result;
+use std::time::Instant;
 
-use crate::cli::map::ArgsGeneric;
+use anyhow::Result;
+use cyto::libraries::GenericLibrary;
+
+use crate::{
+    cli::map::ArgsGeneric,
+    io::{write_features, write_statistics},
+};
+
+use super::{
+    implementor::ibu_map_pairs_paraseq,
+    utils::{build_filepath, delete_empty_path},
+};
+
+#[cfg(feature = "binseq")]
+use super::ibu_map_pairs_binseq;
+
+fn bus(args: ArgsGeneric) -> Result<()> {
+    // Load the input files
+    let (r1, r2) = args.input.to_readers()?;
+    let offset = args.generic.offset();
+
+    let start_time = Instant::now();
+    let target_library = GenericLibrary::from_tsv(args.generic.generic_filepath.into())?;
+    let target_mapper = if args.generic.exact_matching {
+        target_library.into_mapper()
+    } else {
+        target_library.into_corrected_mapper()
+    }?;
+
+    // Define the file path for the output file
+    let output_filepath = build_filepath(&args.output.prefix, None);
+
+    // Write the features to the output file
+    write_features(&args.output, &target_mapper)?;
+
+    let statistics = ibu_map_pairs_paraseq(
+        r1,
+        r2,
+        &output_filepath,
+        target_mapper,
+        Some(offset),
+        args.geometry.into(),
+        args.runtime.num_threads(),
+        args.generic.exact_matching,
+        start_time,
+    )?;
+
+    // Delete the output file if there are no mapped reads
+    delete_empty_path(&output_filepath)?;
+
+    // Write the statistics to the output file
+    write_statistics(&args.output, &statistics)?;
+    Ok(())
+}
+
+#[cfg(feature = "binseq")]
+fn bus_binseq(args: ArgsGeneric) -> Result<()> {
+    let reader = args.binseq.into_reader()?;
+    let offset = args.generic.offset();
+
+    let start_time = Instant::now();
+    let target_library = GenericLibrary::from_tsv(args.generic.generic_filepath.into())?;
+    let target_mapper = if args.generic.exact_matching {
+        target_library.into_mapper()
+    } else {
+        target_library.into_corrected_mapper()
+    }?;
+
+    // Define the file path for the output file
+    let output_filepath = build_filepath(&args.output.prefix, None);
+
+    // Write the features to the output file
+    write_features(&args.output, &target_mapper)?;
+
+    // Open a file handle for the output file
+    let statistics = ibu_map_pairs_binseq(
+        reader,
+        &output_filepath,
+        target_mapper,
+        Some(offset),
+        args.geometry.into(),
+        args.runtime.num_threads(),
+        args.generic.exact_matching,
+        start_time,
+    )?;
+
+    write_statistics(&args.output, &statistics)?;
+    Ok(())
+}
 
 pub fn run(args: ArgsGeneric) -> Result<()> {
-    Ok(())
+    #[cfg(feature = "binseq")]
+    if args.binseq.input.is_some() {
+        return bus_binseq(args);
+    }
+    bus(args)
 }
