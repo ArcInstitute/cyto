a6fb321350118ebe0b6d1000aef38301ef61b9cf
noamteyssier <22600644+noamteyssier@users.noreply.github.com>
2025-04-09 11:27:29 -0700

    refactor: move sort and coutn to utils

--- cyto-cli/src/commands/workflow/flex.rs
+++ cyto-cli/src/commands/workflow/flex.rs
@@ -1,31 +1,9 @@
 use anyhow::Result;
 use glob::glob;
 
-use crate::cli::ibu::ArgsCount;
-use crate::cli::ibu::ArgsSort;
+use super::utils::sort_and_count;
 use crate::cli::workflow::FlexMappingCommand;
-use crate::commands::ibu as ibu_command;
 use crate::commands::map as map_command;
-
-fn sort_and_count(ibu_path: &str, prefix: &str) -> Result<()> {
-    let sort_path = ibu_path.replace(".ibu", ".sort.ibu");
-    let sort_args = ArgsSort::from_wf_path(ibu_path, &sort_path, 1);
-
-    eprintln!(">> Sorting {ibu_path} -> {sort_path}");
-    ibu_command::sort::run(&sort_args)?;
-
-    eprintln!(">> Removing unsorted file: {ibu_path}");
-    std::fs::remove_file(ibu_path)?;
-
-    let feature_path = format!("{prefix}.features.tsv");
-    let count_path = sort_path.replace(".sort.ibu", ".counts.tsv");
-    let count_args = ArgsCount::from_wf_path(&sort_path, &count_path, &feature_path, 1);
-
-    eprintln!(">> Counting {sort_path} -> {count_path}");
-    ibu_command::count::run(&count_args)?;
-
-    Ok(())
-}
 
 pub fn run(args: &FlexMappingCommand) -> Result<()> {
     eprintln!(">> Running Flex Mapping Command");
