052b91ba1aa3ef0a9159f15a6cf52a58cd5dfb5b
noamteyssier <22600644+noamteyssier@users.noreply.github.com>
2025-08-04 15:44:48 -0700

    refactor(workflow): downgrade file removal to debug to reduce info spam in log

--- crates/cyto-workflow/src/utils.rs
+++ crates/cyto-workflow/src/utils.rs
@@ -1,16 +1,14 @@
 use std::path::Path;
 
-use anyhow::Context;
-use anyhow::Result;
 use anyhow::bail;
-use glob::glob;
-
+use anyhow::{Context, Result};
 use cyto_cli::{
     ibu::{ArgsBarcode, ArgsCount, ArgsSort, ArgsUmi},
     workflow::ArgsWorkflow,
 };
 use cyto_ibu_barcode_correct::Whitelist;
-use log::info;
+use glob::glob;
+use log::{debug, error, info};
 
 pub fn identify_ibu_files<P: AsRef<Path>>(outdir: P) -> Result<Vec<String>> {
     let ibu_files = glob(&format!("{}/ibu/*.ibu", outdir.as_ref().display()))?
@@ -50,7 +48,7 @@ pub fn ibu_steps<P: AsRef<Path>>(
     info!("Sorting {ibu_path} -> {sort_path}");
     cyto_ibu_sort::run(&sort_args)?;
 
-    info!("Removing unsorted file: {ibu_path}");
+    debug!("Removing unsorted file: {ibu_path}");
     std::fs::remove_file(ibu_path)?;
 
     if !wf_args.skip_barcode {
@@ -64,22 +62,23 @@ pub fn ibu_steps<P: AsRef<Path>>(
         let barcode_args =
             ArgsBarcode::from_wf_path(&sort_path, &bc_path, &wf_args.whitelist, bc_log);
         let Some(whitelist) = whitelist else {
+            error!("Whitelist is required for barcode correction");
             bail!("Whitelist is required for barcode correction");
         };
 
         info!("Barcode Correcting {sort_path} -> {bc_path}");
         cyto_ibu_barcode_correct::run_with_prebuilt_whitelist(&barcode_args, whitelist)?;
 
-        info!("Removing uncorrected file: {sort_path}");
+        debug!("Removing uncorrected file: {sort_path}");
         std::fs::remove_file(&sort_path)?;
 
         sort_path = bc_path.replace(".barcode.ibu", ".barcode.sort.ibu");
-        info!("Sorting corrected file: {bc_path} -> {sort_path}");
+        info!("Sorting barcode corrected file: {bc_path} -> {sort_path}");
 
         let sort_args = ArgsSort::from_wf_path(&bc_path, &sort_path, 1);
         cyto_ibu_sort::run(&sort_args)?;
 
-        info!("Removing unsorted file: {bc_path}");
+        debug!("Removing unsorted file: {bc_path}");
         std::fs::remove_file(&bc_path)?;
     }
 
@@ -96,16 +95,16 @@ pub fn ibu_steps<P: AsRef<Path>>(
         info!("UMI Correcting {sort_path} -> {umi_path}");
         cyto_ibu_umi_correct::run(&umi_args)?;
 
-        info!("Removing uncorrected file: {sort_path}");
+        debug!("Removing uncorrected file: {sort_path}");
         std::fs::remove_file(&sort_path)?;
 
         sort_path = umi_path.replace(".umi.ibu", ".umi.sort.ibu");
-        info!("Sorting corrected file: {umi_path} -> {sort_path}");
+        info!("Sorting UMI corrected file: {umi_path} -> {sort_path}");
 
         let sort_args = ArgsSort::from_wf_path(&umi_path, &sort_path, 1);
         cyto_ibu_sort::run(&sort_args)?;
 
-        info!("Removing unsorted file: {umi_path}");
+        debug!("Removing unsorted file: {umi_path}");
         std::fs::remove_file(&umi_path)?;
     }
 
