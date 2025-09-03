4181694c88d0994b27926cb73512e00e97c557b1
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-09-02 19:14:50 -0700

    feat(io): added a transparent writer based on extension

--- crates/cyto-io/src/lib.rs
+++ crates/cyto-io/src/lib.rs
@@ -1,5 +1,8 @@
 mod utils;
 mod write;
 
-pub use utils::{match_input, match_input_transparent, match_output, match_output_stderr};
+pub use utils::{
+    match_input, match_input_transparent, match_output, match_output_stderr,
+    match_output_transparent,
+};
 pub use write::{open_file_handle, validate_output_directory, write_features, write_statistics};
