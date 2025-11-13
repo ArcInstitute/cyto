ad17e7398f9a33de339e756e678289c6aa6171d3
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-11-13 15:00:54 -0800

    dep: update pycyto

--- crates/cyto-cli/src/workflow/mod.rs
+++ crates/cyto-cli/src/workflow/mod.rs
@@ -8,7 +8,7 @@ use super::{ArgsCrispr, ArgsGex};
 
 pub const VERSION_GEOMUX: &str = "0.5.4";
 pub const VERSION_CELL_FILTER: &str = "0.1.2";
-pub const VERSION_PYCYTO: &str = "0.1.8";
+pub const VERSION_PYCYTO: &str = "0.1.10";
 
 #[derive(Subcommand, Debug)]
 pub enum WorkflowCommand {
