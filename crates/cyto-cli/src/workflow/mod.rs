f772e948d7a454941c44505b6edb7666bfe2db5c
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-11-13 15:01:18 -0800

    dep: update geomux

--- crates/cyto-cli/src/workflow/mod.rs
+++ crates/cyto-cli/src/workflow/mod.rs
@@ -6,7 +6,7 @@ use log::{debug, error};
 
 use super::{ArgsCrispr, ArgsGex};
 
-pub const VERSION_GEOMUX: &str = "0.5.4";
+pub const VERSION_GEOMUX: &str = "0.5.5";
 pub const VERSION_CELL_FILTER: &str = "0.1.2";
 pub const VERSION_PYCYTO: &str = "0.1.10";
 
